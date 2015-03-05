891c1db146370f4eed13cf3a164d9ee05f882206
kostapc <kostapc@gmail.com>
2015-03-06 02:09:34 +0300

    initial commit

---
+++ src/net/c0fe/experiments/reflection/CryptByteCodeLoader.java
@@ -0,0 +1,30 @@
+package net.c0fe.experiments.reflection;
+
+import java.io.*;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+
+/**
+ * User: kostapc
+ */
+
+public class CryptByteCodeLoader extends ClassLoader {
+
+    private String fullClassName;
+    private byte[] classByteCode;
+
+    public CryptByteCodeLoader(String inFullClassName, byte[] inClassByteCode) {
+        fullClassName = inFullClassName;
+        classByteCode = inClassByteCode;
+    }
+
+    @Override
+    public Class<?> loadClass(String name) throws ClassNotFoundException {
+        if(!name.equals(fullClassName)) {
+            return findSystemClass(name);
+        }
+        return defineClass(name, classByteCode, 0, classByteCode.length);
+    }
+
+}
